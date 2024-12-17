ba67889ce67455dd9d71b149717ae7cd4f170565
KenArsen <kenzhegulov.kgz@gmail.com>
2024-12-17 23:04:54 +0600

    Changed docker settings

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 
-set -e  # Прекратить выполнение при любой ошибке
-
 # Debug: Вывод информации о среде
-echo "Starting entrypoint script..."
-
-# Debug: Убедиться, что зависимости установлены
-echo "Checking installed dependencies..."
-poetry show
+echo "Starting entrypoint script with Poetry..."
 
 # Применение миграций
 echo "Applying database migrations..."
-python manage.py migrate
+poetry run python manage.py migrate
 
-# Сбор статических файлов
-echo "Collecting static files..."
-python manage.py collectstatic --no-input
+## Сбор статических файлов
+#echo "Collecting static files..."
+#poetry run python manage.py collectstatic --no-input
 
 # Запуск Gunicorn-сервера
 echo "Starting Gunicorn server..."
-exec gunicorn --bind :8000 config.wsgi:application
+exec poetry run gunicorn --bind :8000 config.wsgi:application
