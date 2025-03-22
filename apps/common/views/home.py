d68d4ae5b14552034c9386b6488a418e638ffc08
KenArsen <kenzhegulov.kgz@gmail.com>
2025-03-22 12:20:25 +0600

    Changing tepmlates

--- apps/common/views/home.py
+++ apps/common/views/home.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
-from apps.product.models import Category
+from apps.product.models import Category, Product
 
 
 def home_view(request):
-    context = {"categories": Category.objects.all()}
+    products = Product.objects.all()[:10]
+    context = {"categories": Category.objects.all(), "products": products}
     return render(request, "index.html", context=context)
