f6dcc549b7681447f3611f1dc39e718964301d13
KenArsen <kenzhegulov.kgz@gmail.com>
2025-03-23 17:01:01 +0600

    Изменил стили шаблонови и начал написать логику поиска

--- apps/common/views/home.py
+++ apps/common/views/home.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.views.generic import TemplateView
 
 from apps.product.models import Category, Product
 
 
-def home_view(request):
-    products = Product.objects.all()[:10]
-    context = {"categories": Category.objects.all(), "products": products}
-    return render(request, "index.html", context=context)
+class HomeView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        search_query = self.request.GET.get("search", "")
+
+        # Базовый QuerySet
+        queryset = Product.objects.all()
+
+        # Применяем фильтры поиска, если они указаны
+        if search_query:
+            queryset = queryset.filter(name__icontains=search_query)
+
+        # Настройка пагинации
+        paginator = Paginator(queryset, 10)  # 10 элементов на страницу
+        page = self.request.GET.get("page")
+
+        try:
+            paginated_objects = paginator.page(page)
+        except PageNotAnInteger:
+            # Если page не целое число, отображаем первую страницу
+            paginated_objects = paginator.page(1)
+        except EmptyPage:
+            # Если page больше чем количество страниц, отображаем последнюю
+            paginated_objects = paginator.page(paginator.num_pages)
+
+        # Добавляем пагинированные объекты в контекст
+        context["boycotted_products"] = paginated_objects
+        context["categories"] = Category.objects.all()
+
+        # Сохраняем параметры поиска для сохранения их при пагинации
+        context["search_query"] = search_query
+
+        return context
--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from apps.common.views.home import home_view
+from apps.common.views.home import HomeView
 
 urlpatterns = [
-    path("", home_view, name="home"),
+    path("", HomeView.as_view(), name="home"),
     path("admin/", admin.site.urls),
     path("i18n/", include("django.conf.urls.i18n")),
     path("accounts/", include("apps.account.urls", namespace="account")),
