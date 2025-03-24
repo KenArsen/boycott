ef4f0749cef6591f1bd691740bc8e50666ce02dc
KenArsen <kenzhegulov.kgz@gmail.com>
2025-03-24 17:20:39 +0600

    Added home address in index.html and Class-based views for Category

--- apps/product/views/category.py
+++ apps/product/views/category.py
@@ -5,12 +5,12 @@ from apps.product.models import Category
 
 class CategoryListView(ListView):
     model = Category
-    template_name = "product/category_list.html"
+    template_name = "pages/category.html"
     context_object_name = "categories"
     paginate_by = 10
 
 
 class CategoryDitailView(DetailView):
     model = Category
-    template_name = "product/category_detail.html"
+    template_name = "pages/category.html"
     context_object_name = "category"
